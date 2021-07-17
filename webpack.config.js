b3e3c9c9c113368515a45738f636b328ea3cee94
windowdotonload <275981471@qq.com>
2021-07-17 22:22:12 +0800

    mod

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index'),
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '',
     filename: "bundle.js",
     chunkFilename: '[name].js'
   },
@@ -25,6 +24,7 @@ module.exports = {
       },
       {
         test: /\.(jpeg|png|jpg)$/,
+
         loader: 'url-loader',
         options: {
           // 图片小于8kb，转化为base64处理
@@ -33,7 +33,9 @@ module.exports = {
           limit: 8 * 1024,
           // 给图片重命名
           // 取hash值前十位，[ext]文件原扩展名
-          name: '[Hash:10].[ext]'
+          esModule: false,
+          name: '[hash:10].[ext]',
+          outputPath: 'assets'
         },
       },
       {
@@ -62,10 +64,12 @@ module.exports = {
     extensions: ['.json', '.js', '.jsx']
   },
   devtool: 'source-map',
+  // 开发服务器
+  // 只会在内存中编译打包，不会有任何实际物理上的输出
+  // 启用devServer指令，webpack-dev-server   npx webpack-dev-server
   devServer: {
-    contentBase: path.join(__dirname, '/dist/'),
-    inline: true,
-    host: 'localhost',
+    contentBase: path.join(__dirname),
+    compress: true,
     port: 8080,
   }
 };
